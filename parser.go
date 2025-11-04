714883c92e4adade2eb3a6973b0b3d7361e533ad
Daniel Ortiz <dortiz@krakend.io>
2025-11-04 10:39:18 +0000

    Replace mapstructure dep for the new maintained package.
    Signed-off-by: Daniel Ortiz <dortiz@krakend.io>

--- parser.go
+++ parser.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/mitchellh/mapstructure"
+	"github.com/go-viper/mapstructure/v2"
 
 	bf "github.com/krakend/bloomfilter/v2/krakend"
 	botdetector "github.com/krakend/krakend-botdetector/v2/krakend"
